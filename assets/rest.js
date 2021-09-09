c5936b58516502b2b2a99da7791ec59241fe565e
Giselle556 <giselleborbolla@yahoo.com>
2021-09-09 11:31:43 -0700

    api

--- assets/rest.js
+++ assets/rest.js
@@ -1,3 +1,16 @@
 var requestUrl = 'https://api.documenu.com/v2/restaurants/search/geo';
 
 var responseText = document.getElementById('response-text');
+
+const params = {
+    "lat": "40.68919",
+    "distance": "1",
+    "lon": "-73.992378",
+    "page": "1"
+}
+let result = await Documenu.MenuItems.searchGeo(params)
+
+Documenu.Restaurants.searchGeo(params)
+.then(res=> {
+    //window.$log.trace(res);
+});
