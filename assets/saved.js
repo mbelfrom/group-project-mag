0d50b49f454257fae79936e051a9b678a43fb57f
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-09-09 12:04:14 -0700

    add anchor tags to home page

--- assets/saved.js
+++ assets/saved.js
@@ -1,17 +1,14 @@
 //When the user clicks the retrieve button a modal carousel is displayed
-$("#lanuchModal").click(function() {
-    $(".modal").addClass("is-active"); 
-  });
-  
-  $(".modal-close").click(function() {
-    $(".modal").removeClass("is-active");
-  });
-  
-  $("#closebtn").click(function() { 
-    $(".modal").removeClass("is-active");
-  });
+$("#lanuchModal").click(function () {
+  $(".modal").addClass("is-active");
+});
+
+$(".modal-close").click(function () {
+  $(".modal").removeClass("is-active");
+});
+
+$("#closebtn").click(function () {
+  $(".modal").removeClass("is-active");
+});
 
 //User Access to local storage
-
-var saveRecipe = localStorage.setItem(this);
-var accessRecipes = 
