2c7e88571a2953fcb0a6e45f3944806fb4f0fb14
Annalisa <annalisa.bauer1@gmail.com>
2021-09-10 11:24:29 -0700

    resturant api

--- assets/rest.js
+++ assets/rest.js
@@ -1,46 +1,52 @@
-var requestUrl = 'https://api.documenu.com/v2/restaurant/4072702673999819?key=40e793cde7d7318555e7aba7e6fbe541';
+var requestUrl = "https://api.documenu.com/v2/menuitems/search/geo?";
+//   lat=40.688072&lon=-73.997385&distance=1&search=buffalo%20chicken";
 
-function getData (){
-    fetch (requestUrl)
-    .then (function(response){
-        return response.json()
+// 
 
-    })
-    .then (function(data){
-        console.log(data)
-        const lat = data.result.geo.lat
-        const lon = data.result.geo.lon
-        const name = data.result.restaurant_name
-        const web = data.result.restaurant_website
+window.navigator.geolocation.getCurrentPosition(function (geoData) {
+    console.log(geoData);
+    
+    
+        fetch (requestUrl)
+        .then (function(response){
+            return response.json()
+            
+        })
+        .then (function(data){
+            console.log(data)
+            
+            const urlParams = new URLSearchParams(window.location.search);
+            const protein = urlParams.get("protein");
+            
+            const lat = data.result.geo.lat
+            const lon = data.result.geo.lon
+            const name = data.result.restaurant_name
+            const web = data.result.restaurant_website
+            
+            const params = new URLSearchParams({
+              app_key: "35e07563b7ad4eac86d6ccfd97946762",
+              lat: lat,
+              lon: lon,
+              distance: 1,
+              search: protein,
+            });
+            
+            const webDiv = document.getElementById("webDiv");
+            const webElement = document.createElement("a");
+            const link = document.createTextNode('Check out your nearest restaurant!');
+            webElement.appendChild(link);
+            webElement.title = "Restaurant URL";
+            webElement.href = (`${web}`);
+            webDiv.append(webElement);
+            
+            const restname = document.getElementById('rest-name');
+            const nameElement = document.createElement('h1');
+            nameElement.innerText = `${name}`;
+            restname.append(nameElement);
+            
+        })
+    });
 
 
-        const webDiv = document.getElementById("webDiv");
-        const webElement = document.createElement("a");
-        const link = document.createTextNode('Check out your nearest restaurant!');
-        webElement.appendChild(link);
-        webElement.title = "Restaurant URL";
-        webElement.href = (`${web}`);
-        webDiv.append(webElement);
 
-        const restname = document.getElementById('rest-name');
-        const nameElement = document.createElement('h1');
-        nameElement.innerText = `${name}`;
-        restname.append(nameElement);
 
-    })
-}
-getData ()
-// var responseText = document.getElementById('response-text');
-
-// const params = {
-//     "lat": "40.68919",
-//     "distance": "1",
-//     "lon": "-73.992378",
-//     "page": "1"
-// }
-// let result = await Documenu.MenuItems.searchGeo(params)
-
-// Documenu.Restaurants.searchGeo(params)
-// .then(res=> {
-//     //window.$log.trace(res);
-// });
