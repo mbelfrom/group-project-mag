b807a64cef77db4753bcad243570cf913f62ec28
Giselle556 <giselleborbolla@yahoo.com>
2021-09-09 18:58:16 -0700

    href link updated

--- assets/rest.js
+++ assets/rest.js
@@ -1,16 +1,41 @@
-var requestUrl = 'https://api.documenu.com/v2/restaurants/search/geo';
+var requestUrl = 'https://api.documenu.com/v2/restaurant/4072702673999819?key=f1f6dd6fce22010400139ef214b7da62';
 
-var responseText = document.getElementById('response-text');
+function getData (){
+    fetch (requestUrl)
+    .then (function(response){
+        return response.json()
 
-const params = {
-    "lat": "40.68919",
-    "distance": "1",
-    "lon": "-73.992378",
-    "page": "1"
+    })
+    .then (function(data){
+        console.log(data)
+        const lat = data.result.geo.lat
+        const lon = data.result.geo.lon
+        const name = data.result.restaurant_name
+
+        const restname = document.getElementById('rest-name');
+        const nameElement = document.createElement('h1');
+        nameElement.innerText = `${name}`;
+        restname.append(nameElement);
+        
+        const restDiv = document.getElementById("rest-div");
+        const restElement = document.createElement('a');
+        const link = document.createTextNode('Check out your nearest restaurant!');
+        restElement.appendChild(link);
+        restDiv.append(restElement);
+    })
 }
-let result = await Documenu.MenuItems.searchGeo(params)
+getData ()
+// var responseText = document.getElementById('response-text');
 
-Documenu.Restaurants.searchGeo(params)
-.then(res=> {
-    //window.$log.trace(res);
-});
+// const params = {
+//     "lat": "40.68919",
+//     "distance": "1",
+//     "lon": "-73.992378",
+//     "page": "1"
+// }
+// let result = await Documenu.MenuItems.searchGeo(params)
+
+// Documenu.Restaurants.searchGeo(params)
+// .then(res=> {
+//     //window.$log.trace(res);
+// });
